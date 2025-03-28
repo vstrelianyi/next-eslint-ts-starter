1a5af06cf03a1e34f8f550cfd72a910dd5f47b37
viktor_strelianyi <chmpain@gmail.com>
2025-03-28 14:46:42 +0200

    - eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,11 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
